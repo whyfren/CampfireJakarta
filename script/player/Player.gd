450bd35efce3e3e0f2e82ad826520e06009fc5b9
bagas-aja <biboyhd10@gmail.com>
2026-03-01 08:56:22 +0700

    attack player prototype

--- script/player/Player.gd
+++ script/player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 # ==============================
-# NORMAL MOVEMENT SETTINGS
+# NORMAL MOVEMENT
 # ==============================
 export var speed = 200
 export var run_multiplier = 1.8
@@ -9,20 +9,22 @@ export var gravity = 1000
 export var jump_force = -450
 
 # ==============================
-# SWIMMING SETTINGS
+# SWIMMING
 # ==============================
 export var swim_speed = 150
 export var swim_fast_multiplier = 1.7
 
-# oxigen
-var oxygen := 100.0
-var oxygen_drain_rate := 10.0
+# ==============================
+# ATTACK
+# ==============================
+export var attack_duration = 0.2
+export var attack_cooldown = 0.4
 
-# movement veriable
 var velocity = Vector2.ZERO
 var is_in_water = false
 var facing_right = true
-var is_running = false
+var is_attacking = false
+var can_attack = true
 
 # ==============================
 # INVENTORY
@@ -35,12 +37,13 @@ var inventory = []
 # ==============================
 func _physics_process(delta):
 
-	if is_in_water:
-		swim_movement()
-		# drain oksigen
-		if is_running:
-			oxygen -= (oxygen_drain_rate * 2) * delta
+	handle_attack_input()
+
+	if not is_attacking:
+		if is_in_water:
+			swim_movement()
 		else:
+<<<<<<< Updated upstream
 			oxygen -= oxygen_drain_rate * delta
 		print("oxygen : ", oxygen)
 		GameState.oxygen = oxygen
@@ -54,13 +57,16 @@ func _physics_process(delta):
 		print("oxygen : ", oxygen)
 		GameState.oxygen = oxygen
 		oxygen = min(oxygen + 20 * delta, 100)
+=======
+			normal_movement(delta)
+>>>>>>> Stashed changes
 
 	velocity = move_and_slide(velocity, Vector2.UP)
 	handle_animation()
 
 
 # ==============================
-# NORMAL MOVEMENT (WALK & RUN)
+# NORMAL MOVEMENT
 # ==============================
 func normal_movement(delta):
 
@@ -75,19 +81,14 @@ func normal_movement(delta):
 
 	var current_speed = speed
 
-	# RUN
 	if Input.is_action_pressed("lari") and direction != 0:
 		current_speed *= run_multiplier
-		var is_running = true
-	else:
-		var is_running = false
 
 	velocity.x = direction * current_speed
 
 	if direction != 0:
 		facing_right = direction > 0
 
-	# JUMP
 	if is_on_floor() and Input.is_action_just_pressed("lompat"):
 		velocity.y = jump_force
 
@@ -111,12 +112,8 @@ func swim_movement():
 
 	var current_speed = swim_speed
 
-	# SWIM FASTER (lari di air)
 	if Input.is_action_pressed("lari") and (x_dir != 0 or y_dir != 0):
 		current_speed *= swim_fast_multiplier
-		var is_running = true
-	else:
-		var is_running = false
 
 	velocity.x = x_dir * current_speed
 	velocity.y = y_dir * current_speed
@@ -126,27 +123,67 @@ func swim_movement():
 
 
 # ==============================
-# ANIMATION HANDLER
+# ATTACK SYSTEM (GODOT 3 STYLE)
+# ==============================
+func handle_attack_input():
+	if Input.is_action_just_pressed("attack") and can_attack:
+		start_attack()
+
+
+func start_attack():
+	is_attacking = true
+	can_attack = false
+	velocity = Vector2.ZERO
+
+	$AnimatedSprite.play("attack")
+	enable_hitbox()
+
+	yield(get_tree().create_timer(attack_duration), "timeout")
+	disable_hitbox()
+
+	is_attacking = false
+
+	yield(get_tree().create_timer(attack_cooldown), "timeout")
+	can_attack = true
+
+
+func enable_hitbox():
+	$AttackArea.monitoring = true
+	$AttackArea/CollisionShape2D.disabled = false
+
+
+func disable_hitbox():
+	$AttackArea.monitoring = false
+	$AttackArea/CollisionShape2D.disabled = true
+
+
+# ==============================
+# ANIMATION SYSTEM
 # ==============================
 func handle_animation():
 
 	$AnimatedSprite.flip_h = not facing_right
 
-	# ==========================
-	# SWIMMING MODE
-	# ==========================
+	# Geser hitbox sesuai arah
+	if facing_right:
+		$AttackArea.position.x = abs($AttackArea.position.x)
+	else:
+		$AttackArea.position.x = -abs($AttackArea.position.x)
+
+	# ATTACK PRIORITY
+	if is_attacking:
+		return
+
+	# ===== SWIM MODE =====
 	if is_in_water:
 
-		# TURUN
 		if velocity.y > 0:
 			if $AnimatedSprite.animation != "bawah":
 				$AnimatedSprite.play("bawah")
 			return
 
-		# BERGERAK
 		if velocity.x != 0 or velocity.y < 0:
 
-			# Swim Faster
 			if Input.is_action_pressed("lari"):
 				if $AnimatedSprite.animation != "swim_faster":
 					$AnimatedSprite.play("swim_faster")
@@ -155,15 +192,12 @@ func handle_animation():
 					$AnimatedSprite.play("swiming")
 			return
 
-		# IDLE DI AIR
 		if $AnimatedSprite.animation != "swim":
 			$AnimatedSprite.play("swim")
 		return
 
 
-	# ==========================
-	# NORMAL MODE
-	# ==========================
+	# ===== LAND MODE =====
 	if not is_on_floor():
 		if $AnimatedSprite.animation != "jump":
 			$AnimatedSprite.play("jump")
@@ -194,8 +228,16 @@ func exit_water():
 
 
 # ==============================
-# INVENTORY SYSTEM
+# INVENTORY
 # ==============================
 func add_item(item_name):
 	inventory.append(item_name)
 	print("Inventory:", inventory)
+
+
+# ==============================
+# ATTACK HIT DETECTION
+# ==============================
+func _on_AttackArea_body_entered(body):
+	if body.has_method("take_damage"):
+		body.take_damage(1)
